e643feb0a1a57d86aeb3203b6b1b7e5eb08e08e6
nilesh_vernost <nilesh.thote@vernost.in>
2023-09-16 11:35:43 +0530

    exception handling code applied

--- execption_handling.php
+++ execption_handling.php
@@ -1,3 +1,31 @@
 <?php
-    echo 'Hello';
+    class MyException extends Exception{
+        function errorMessage(){
+            $error = "My Exception Message : ".$this->getMessage()." Error on line Number : ".$this->getLine();
+            return $error;
+        }
+    }
+
+    function division($n){
+        try {
+            if ($n <= 0) {
+                throw new Exception("Enter The valid number");
+            }
+            if($n == 3){
+                throw new MyException("Number is 3");
+            }
+            $div = 2/$n;
+            echo $div;
+        } catch (MyException $e) {
+            echo $e->errorMessage();
+        }catch (Exception $e) {
+            echo $e->getMessage();
+        }finally{
+            echo "finally";
+        }
+    }
+
+    division(-1);
+    division(3);
+    division(-1);
 ?>
